2b3f1d1f8eabac9d697a7fb83336d7e233f86868
garu097 <garu097@gmail.com>
2025-07-14 16:48:27 +0700

    update: wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,24 +1,24 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
-local color_frame = 'gray'
-
+local color_frame = 'pink'
 -- window setting
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 28
+config.window_background_opacity = 0.97
+config.macos_window_background_blur = 20
 config.window_padding = {
-    left = 2,
-    right = 2,
+    left = 4,
+    right = 4,
     top = 8,
-    bottom = 8,
+    bottom = 4,
 }
 
 config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font("0xProto Nerd Font Mono", {weight = "Bold",stretch="Expanded"})
-config.font_size = 13
-config.line_height = 1.2
+config.font = wezterm.font("0xProto Nerd Font Mono", {weight = "Bold", stretch="Expanded"})
+config.font_size = 14
+config.line_height = 1.4
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-
+config.initial_rows = 40
+config.initial_cols = 280
 config.window_frame = {
     border_left_width = "0.28cell",
     border_right_width = "0.28cell",
@@ -29,12 +29,15 @@ config.window_frame = {
     border_bottom_color = color_frame,
     border_top_color = color_frame,
 }
+
 config.default_cursor_style = "BlinkingUnderline"
 config.cursor_thickness = 2
 config.keys = {
     { key = 'f', mods= 'CTRL|CMD', action = wezterm.action.ToggleFullScreen},
     { key = 'A', mods = 'CTRL|SHIFT', action = wezterm.action.QuickSelect } -- Default QuickSelect key bind (Ctrl-Shift-Space) gets capture by something else
 }
+
+config.window_close_confirmation = 'NeverPrompt'
 
 -- for window platform
 -- config.default_prog = { 'powershell' }

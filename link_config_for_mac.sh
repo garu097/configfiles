49ffa966060e6a57fdb58625cdc97bc4bab850f7
garu097 <garu097@gmail.com>
2026-04-24 17:18:26 +0700

    fix: link pwd

--- link_config_for_mac.sh
+++ link_config_for_mac.sh
@@ -1,7 +1,8 @@
-ln -s ./zsh/config/.zshrc ~/.zshrc
-ln -s ./zsh/powerlevel10k/.p10k.zsh ~/.p10k.zsh
-ln -s ./wezterm/.wezterm.lua ~/.wezterm.lua
-ln -s ./tmux/tmux.conf ~/.tmux.conf
+ln -sf "$(pwd)/zsh/config/.zshrc" ~/.zshrc
+ln -sf "$(pwd)/zsh/powerlevel10k/.p10k.zsh" ~/.p10k.zsh
+ln -sf "$(pwd)/wezterm/.wezterm.lua" ~/.wezterm.lua
+ln -sf "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
+ln -sf "$(pwd)/.opencode" ~/.opencode
 
 chmod +x ./ssh/server_access_ssh.sh           # đảm bảo có quyền thực thi
 ln -s "$(pwd)/ssh/server_access_ssh.sh" ~/server_access_ssh.sh
